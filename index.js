5a7079c374d12d203e69f7c56f960c8859395236
Arnav K <arnavkaushal09@gmail.com>
2024-07-01 17:08:23 +0000

    add ai endpoint

--- index.js
+++ index.js
@@ -2,11 +2,23 @@
  * Importing modules
  */
 const { TwitterApi: X_API } = require("twitter-api-v2");
+const OpenAI = require("openai");
 
 /*
  * Meta values defined as constants
  */
-const X_TOKEN = "todo";
+const X_TOKEN = process.env.X_API_KEY;
+const AI_API_KEY = process.env.AI_API_KEY;
+const PROMPT = `hi`;
+
+/**
+ * Creating new API Client to acesss AI
+ */
+const AI_CLIENT = new OpenAI({
+  apiKey: AI_API_KEY,
+  baseURL: "https://llm.mdb.ai/",
+});
+console.log("[CLIENT] New AI client initiated with specified token.");
 
 /*
  * New X client initialize
@@ -33,7 +45,11 @@ const postMessage = async (content) => {
  * Fetched post content with llm
  */
 const getPostContent = async () => {
-  const content = "";
+  const chatCompletion = await AI_CLIENT.chat.completions.create({
+    messages: [{ role: "user", content: PROMPT }],
+    model: "mixtral-8x7b",
+  });
+  const content = chatCompletion.choices[0]?.message?.content ?? "";
   console.log(
     `[LLM] Contacted LLM to get content and resulted with:- ${content}`,
   );
@@ -49,21 +65,21 @@ const getPostContent = async () => {
    */
   const contentToBePosted = await getPostContent().catch((e) =>
     console.log(
-      `[ERROR] An error occured during executing "getPostContent" method:-\n ${e}`,
+      `[ERROR] An error occured during executing "getPostContent" method:-\n ${e.message}`,
     ),
   );
 
   /**
    * Validation
    */
-  if (contentToBePosted.toString().trim().length == 0) return;
+  if (contentToBePosted?.toString().trim().length == 0) return;
 
   /**
    * Posting resulted content to X
    */
   await postMessage(contentToBePosted).catch((e) =>
     console.log(
-      `[ERROR] An error occured during executing "postMessage" method:-\n ${e}`,
+      `[ERROR] An error occured during executing "postMessage" method:-\n ${e.message}`,
     ),
   );
 })();
